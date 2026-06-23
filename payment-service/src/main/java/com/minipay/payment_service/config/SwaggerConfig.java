98bc05fc9d6c43cb3eea0190da3b428c0a17640a
sammy ocharo <obanyisammy@gmail.com>
2026-06-23 16:00:56 +0300

    fix: swagger config on payment service

--- payment-service/src/main/java/com/minipay/payment_service/config/SwaggerConfig.java
+++ payment-service/src/main/java/com/minipay/payment_service/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -18,7 +19,10 @@ import org.springframework.context.annotation.Configuration;
                         name = "MiniPay Team",
                         email = "support@minipay.com"
                 )
-        )
+        ),
+        servers = {
+                @Server(url = "http://localhost:8080", description = "API Gateway")
+        }
 )
 @SecurityScheme(
         name = "Bearer Authentication",
